cb63276d7bb0447aa3c1deadcea77df15b03a999
JuaniSilva <juanicruzsilva@gmail.com>
2022-05-04 20:06:34 -0300

    remove cards counter

--- src/components/EnergyCalculator.vue
+++ src/components/EnergyCalculator.vue
@@ -1,77 +1,71 @@
 <template>
-  <img src="https://zonaxie.com/img/energy1.svg" alt="" />
-  <p>Rounds {{ round }}</p>
-  <p>{{ counter }} / 10</p>
-  <div class="controls">
-    <Button @click="subsOne" text="-1 Energy" />
-    <Button @click="addOne" text="+1 Energy" />
-    <Button @click="nextRound" text="Next Round" />
-    <Button @click="newGame" text="New Game" />
-  </div>
-  <div class="card-counter">
-    <Button @click="addCards(-1)" text="-1 Card" />
-    <Button @click="addCards(1)" text="+1 Card" />
-  </div>
-  <p>Total Cards:</p>
-  <p>{{ cards }}</p>
+    <img src="https://zonaxie.com/img/energy1.svg" alt="" />
+    <p>Rounds {{ round }}</p>
+    <p>{{ counter }} / 10</p>
+    <div class="controls">
+        <Button @click="subsOne" text="-1 Energy" />
+        <Button @click="addOne" text="+1 Energy" />
+        <Button @click="nextRound" text="Next Round" />
+        <Button @click="newGame" text="New Game" />
+    </div>
 </template>
 <script>
 import Button from "./Button.vue";
 export default {
-  name: "EnergyCalculator",
-  components: {
-    Button,
-  },
-  data() {
-    return {
-      round: 1,
-      counter: 3,
-      cards: 6,
-    };
-  },
-  methods: {
-    addOne() {
-      this.counter < 10 ? this.counter++ : this.counter;
+    name: "EnergyCalculator",
+    components: {
+        Button,
     },
-    subsOne() {
-      this.counter > 0 ? this.counter-- : 0;
+    data() {
+        return {
+            round: 1,
+            counter: 3,
+            cards: 6,
+        };
     },
-    nextRound() {
-      this.round++;
-      this.counter += 2;
-      if (this.counter >= 10) {
-        this.counter = 10;
-      }
-      this.addCards(3);
+    methods: {
+        addOne() {
+            this.counter < 10 ? this.counter++ : this.counter;
+        },
+        subsOne() {
+            this.counter > 0 ? this.counter-- : 0;
+        },
+        nextRound() {
+            this.round++;
+            this.counter += 2;
+            if (this.counter >= 10) {
+                this.counter = 10;
+            }
+            this.addCards(3);
+        },
+        newGame() {
+            this.round = 1;
+            this.counter = 3;
+            this.cards = 6;
+        },
+        addCards(amount) {
+            this.cards += amount;
+            if (this.cards > 12) this.cards = 12;
+        },
     },
-    newGame() {
-      this.round = 1;
-      this.counter = 3;
-      this.cards = 6;
-    },
-    addCards(amount) {
-      this.cards += amount;
-      if (this.cards > 12) this.cards = 12;
-    },
-  },
 };
 </script>
 
 <style scoped>
 img {
-  max-width: 100%;
-  width: 50px;
-  border: 1px solid #2c3e50;
-  border-radius: 50%;
+    max-width: 100%;
+    width: 50px;
+    border: 1px solid #2c3e50;
+    border-radius: 50%;
 }
 .controls {
-  display: flex;
-  justify-content: space-evenly;
+    display: flex;
+    justify-content: space-evenly;
 }
 .card-counter {
-  margin-top: 0.75rem;
+    margin-top: 0.75rem;
 }
 p {
-  margin-top: 0.5rem;
+    margin-top: 0.5rem;
 }
 </style>
--- src/components/RankedTracker.vue
+++ src/components/RankedTracker.vue
@@ -1,37 +1,41 @@
 <template>
-  <Button @click="showTracker = !showTracker" :text="showTracker ? 'Hidde Stats': 'Show Stats' "/>
-  <div class="wrapper" v-if="showTracker">
-    <form @submit.prevent >
-      <label for="trophies">Trophies +/-</label><br />
-      <input id="trophies" type="number" v-model.number="trophies" />
-      <div class="container">
-        <Button @click="addWin" text="Add Win" />
-        <Button @click="addDraw" text="Draw" />
-        <Button @click="addLose" text="Add Lose" />
-      </div>
-    </form>
-    <label for="total-trophies">Current Trophies</label><br />
-    <div class="small-container">
-      <input
-        class="total-trophies"
-        id="total-trophies"
-        v-model.number="totalTrophies"
-      />
-      <img
-        src="https://www.pinclipart.com/picdir/big/355-3556122_4-succeed-vector-trophy-icon-png-clipart.png"
-        alt=""
-      />
+    <Button
+        @click="showTracker = !showTracker"
+        class="toggle-button"
+        :text="showTracker ? 'Hidde Stats' : 'Show Stats'"
+    />
+    <div class="wrapper" v-if="showTracker">
+        <form @submit.prevent>
+            <label for="trophies">Trophies +/-</label><br />
+            <input id="trophies" type="number" v-model.number="trophies" />
+            <div class="container">
+                <Button @click="addWin" text="Add Win" />
+                <Button @click="addDraw" text="Draw" />
+                <Button @click="addLose" text="Add Lose" />
+            </div>
+        </form>
+        <label for="total-trophies">Current Trophies</label><br />
+        <div class="small-container">
+            <input
+                class="total-trophies"
+                id="total-trophies"
+                v-model.number="totalTrophies"
+            />
+            <img
+                src="https://www.pinclipart.com/picdir/big/355-3556122_4-succeed-vector-trophy-icon-png-clipart.png"
+                alt=""
+            />
+        </div>
+        <div class="container">
+            <p>Wins: {{ wins }}</p>
+            <p>Draws: {{ draws }}</p>
+            <p>Loses: {{ loses }}</p>
+        </div>
+        <p>Win Rate {{ winrate }}%</p>
+        <Button @click="resetStats" text="Reset" />
     </div>
-    <div class="container">
-      <p>Wins: {{ wins }}</p>
-      <p>Draws: {{ draws }}</p>
-      <p>Loses: {{ loses }}</p>
-    </div>
-    <p>Win Rate {{ winrate }}%</p>
-    <Button @click="resetStats" text="Reset" />
-  </div>
-  <!-- <Button @click="buffState" :text="showState" class="buffs"/> -->
-  <!-- <Buffsdebuffs v-if="showBuffs"/> -->
+    <!-- <Button @click="buffState" :text="showState" class="buffs"/> -->
+    <!-- <Buffsdebuffs v-if="showBuffs"/> -->
 </template>
 
 <script>
@@ -39,125 +43,128 @@ import Button from "./Button.vue";
 // import Buffsdebuffs from './Buffsdebuffs.vue'
 
 export default {
-  data() {
-    return {
-      winned: 0,
-      lost: 0,
-      trophies: "",
-      totalTrophies: 0,
-      wins: 0,
-      loses: 0,
-      draws: 0,
-      showState: "Show Buffs & Debuffs",
-      showBuffs: false,
-      showTracker: true,
-    };
-  },
-  components: {
-    Button,
-    // Buffsdebuffs,
-  },
-  methods: {
-    addWin() {
-      this.totalTrophies += this.trophies;
-      this.trophies = "";
-      this.wins++;
-      localStorage.setItem("wins", this.wins);
-      this.saveTrophies();
+    data() {
+        return {
+            winned: 0,
+            lost: 0,
+            trophies: "",
+            totalTrophies: 0,
+            wins: 0,
+            loses: 0,
+            draws: 0,
+            showState: "Show Buffs & Debuffs",
+            showBuffs: false,
+            showTracker: true,
+        };
     },
-    addLose() {
-      this.totalTrophies -= this.trophies;
-      this.trophies = "";
-      this.loses++;
-      localStorage.setItem("loses", this.loses);
-      this.saveTrophies();
+    components: {
+        Button,
+        // Buffsdebuffs,
     },
-    addDraw() {
-      this.trophies = "";
-      this.totalTrophies += this.trophies;
-      this.draws++;
-      localStorage.setItem("draws", this.draws);
-      this.saveTrophies();
+    methods: {
+        addWin() {
+            this.totalTrophies += this.trophies;
+            this.trophies = "";
+            this.wins++;
+            localStorage.setItem("wins", this.wins);
+            this.saveTrophies();
+        },
+        addLose() {
+            this.totalTrophies -= this.trophies;
+            this.trophies = "";
+            this.loses++;
+            localStorage.setItem("loses", this.loses);
+            this.saveTrophies();
+        },
+        addDraw() {
+            this.trophies = "";
+            this.totalTrophies += this.trophies;
+            this.draws++;
+            localStorage.setItem("draws", this.draws);
+            this.saveTrophies();
+        },
+        resetStats() {
+            this.wins = 0;
+            this.loses = 0;
+            this.draws = 0;
+            localStorage.removeItem("wins");
+            localStorage.removeItem("loses");
+            localStorage.removeItem("draws");
+        },
+        saveTrophies() {
+            localStorage.setItem("trophies", this.totalTrophies);
+        },
+        buffState() {
+            this.showBuffs = !this.showBuffs;
+            if (this.showBuffs) {
+                this.showState = "Close";
+            } else {
+                this.showState = "Show Buffs & Debuffs";
+            }
+        },
     },
-    resetStats() {
-      this.wins = 0;
-      this.loses = 0;
-      this.draws = 0;
-      localStorage.removeItem("wins");
-      localStorage.removeItem("loses");
-      localStorage.removeItem("draws");
+    computed: {
+        winrate() {
+            if (this.loses === 0 && this.wins === 0) return "0.00";
+            return ((this.wins / (this.wins + this.loses)) * 100).toFixed(2);
+        },
     },
-    saveTrophies() {
-      localStorage.setItem("trophies", this.totalTrophies);
+    async created() {
+        this.wins = localStorage.getItem("wins")
+            ? parseInt(localStorage.getItem("wins"))
+            : 0;
+        this.loses = localStorage.getItem("loses")
+            ? parseInt(localStorage.getItem("loses"))
+            : 0;
+        this.draws = localStorage.getItem("draws")
+            ? parseInt(localStorage.getItem("draws"))
+            : 0;
+        this.totalTrophies = localStorage.getItem("trophies")
+            ? parseInt(localStorage.getItem("trophies"))
+            : 0;
     },
-    buffState() {
-      this.showBuffs = !this.showBuffs;
-      if (this.showBuffs) {
-        this.showState = "Close";
-      } else {
-        this.showState = "Show Buffs & Debuffs";
-      }
-    },
-  },
-  computed: {
-    winrate() {
-      if (this.loses === 0 && this.wins === 0) return "0.00";
-      return ((this.wins / (this.wins + this.loses)) * 100).toFixed(2);
-    },
-  },
-  async created() {
-    this.wins = localStorage.getItem("wins")
-      ? parseInt(localStorage.getItem("wins"))
-      : 0;
-    this.loses = localStorage.getItem("loses")
-      ? parseInt(localStorage.getItem("loses"))
-      : 0;
-    this.draws = localStorage.getItem("draws")
-      ? parseInt(localStorage.getItem("draws"))
-      : 0;
-    this.totalTrophies = localStorage.getItem("trophies")
-      ? parseInt(localStorage.getItem("trophies"))
-      : 0;
-  },
 };
 </script>
 <style scoped>
 form {
-  margin: 0.75rem 0;
+    margin: 0.75rem 0;
 }
 .container {
-  margin: 0.75rem 0;
-  display: flex;
-  justify-content: space-evenly;
+    margin: 0.75rem 0;
+    display: flex;
+    justify-content: space-evenly;
 }
 input {
-  color: #2c3e50;
-  background: inherit;
-  border: 1px solid #000;
-  padding: 0.5rem;
+    color: #2c3e50;
+    background: inherit;
+    border: 1px solid #000;
+    padding: 0.5rem;
 }
 input:focus {
-  outline: none;
+    outline: none;
 }
 input::-webkit-outer-spin-button,
 input::-webkit-inner-spin-button {
-  -webkit-appearance: none;
-  margin: 0;
+    -webkit-appearance: none;
+    margin: 0;
+}
+.toggle-button {
+    margin-top: 1rem;
 }
 
 /* Firefox */
 input[type="number"] {
-  -moz-appearance: textfield;
+    -moz-appearance: textfield;
 }
 .total-trophies {
-  border: none;
-  width: 50px;
+    border: none;
+    width: 50px;
 }
 img {
-  width: 25px;
+    width: 25px;
 }
 .buffs {
-  display: flex;
-  margin: 0.75rem auto;
+    display: flex;
+    margin: 0.75rem auto;
 }
 </style>
